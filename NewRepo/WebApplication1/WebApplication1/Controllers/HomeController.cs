a230ac057005ff6c97c640048bfa48e2a9b3917a
arash saba <arash.saba20@gmail.com>
2019-10-05 03:23:18 +0330

    ss

--- NewRepo/WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ NewRepo/WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -23,6 +23,10 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
+        public IActionResult Privacyws()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
